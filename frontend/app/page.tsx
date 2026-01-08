dfa46744688089e8510c5dc09e5240a9f24cf393
Oleksii <kespa71for@gmail.com>
2026-01-08 15:04:17 +0100

    KAN-17: update: created test pages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,29 +1,3 @@
-import Link from 'next/link';
-
 export default function Home() {
-  return (
-    <div className="container bg-bg-main">
-      <div className="bg-bg-white p-6 rounded-lg shadow-sm">
-        <h1>Header h1</h1>
-        <p className="text-txt-main mt-2">text text text</p>
-        <p className="text-txt-warning mt-2">warning</p>
-        <p className="text-txt-success mt-2">success</p>
-        <button className="btn btn-primary">Save</button>
-        <button className="btn btn-secondary">Cancel</button>
-        <button className="btn btn-primary" disabled>
-          Disabled
-        </button>
-        <div className="p-10 space-y-4">
-          <h2>Header h2</h2>
-          <p>text text text</p>
-          <div className="bg-bg-white p-4 rounded shadow">
-            <h3>Header h3</h3>
-            <p>text text text</p>
-          </div>
-        </div>
-        <div className="border-default p-4">Content</div>
-        <input className="input" placeholder="Hallo" />
-      </div>
-    </div>
-  );
+  return <div>Main page</div>;
 }
--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -1,50 +1,33 @@
 'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as z from 'zod';
-
-import { Button, FormWrapper, Input } from '@/components/ui';
-
-const loginSchema = z.object({
-  email: z.string().email('Invalid email address').min(1, 'Email is required'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-});
-
-type LoginInputs = z.infer<typeof loginSchema>;
 
 export default function Page() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginInputs>({
-    resolver: zodResolver(loginSchema),
-  });
-
-  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
-    console.log('Validated Data:', data);
-  };
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
       <div className="w-full max-w-md rounded-xl bg-white p-8 shadow-lg">
         <h1 className="mb-6 text-2xl font-bold text-gray-800 text-center">Login</h1>
-
-        <FormWrapper onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            label="Email"
-            placeholder="write email"
-            error={errors.email?.message}
-            {...register('email')}
-          />
-          <Input
-            label="Password"
-            placeholder="write password"
-            error={errors.password?.message}
-            {...register('password')}
-          />
-          <Button type="submit">Sign in</Button>
-        </FormWrapper>
+        <div className="container bg-bg-main">
+          <div className="bg-bg-white p-6 rounded-lg shadow-sm">
+            <h1>Header h1</h1>
+            <p className="text-txt-main mt-2">text text text</p>
+            <p className="text-txt-warning mt-2">warning</p>
+            <p className="text-txt-success mt-2">success</p>
+            <button className="btn btn-primary">Save</button>
+            <button className="btn btn-secondary">Cancel</button>
+            <button className="btn btn-primary" disabled>
+              Disabled
+            </button>
+            <div className="p-10 space-y-4">
+              <h2>Header h2</h2>
+              <p>text text text</p>
+              <div className="bg-bg-white p-4 rounded shadow">
+                <h3>Header h3</h3>
+                <p>text text text</p>
+              </div>
+            </div>
+            <div className="border-default p-4">Content</div>
+            <input className="input" placeholder="Hallo" />
+          </div>
+        </div>
       </div>
     </div>
   );
