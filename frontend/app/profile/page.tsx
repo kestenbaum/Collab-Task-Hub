fae7ff694277cdc39010de1b91a2e53cdd812aad
Oleksii <kespa71for@gmail.com>
2026-01-13 14:42:54 +0100

    KAN-38 Update: Frontend User Page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -32,32 +32,29 @@ export default function ProfilePage() {
   return (
     <section className="min-h-screen flex flex-col bg-[var(--color-bg-main)] p-6">
       <div className="grid grid-cols-2 gap-6 w-full [&>*]:w-full">
-        <div className="flex h-full">
-          <Wrapper>
-            <span className="border border-[var(--color-border-default)] grid h-20 w-20 place-items-center rounded-full font-medium">
-              {initialName}
-            </span>
-            <ul>
-              <li className="mt-1 text-gray-900">
-                <span>User ID: {user.id}</span>
-              </li>
-              <li className="mt-1 text-gray-900">
-                <span>User Email: {user.email}</span>
-              </li>
-              <li className="mt-1 text-gray-900">
-                <span>User Name: {user.name}</span>
-              </li>
-            </ul>
-          </Wrapper>
-        </div>
-        <div className="flex h-full w-1/2">
-          <Wrapper>
-            User Project: null
-            <Button onClick={handleLogout} variant="danger">
-              Logout
-            </Button>
-          </Wrapper>
-        </div>
+        <Wrapper>
+          <span className="border border-[var(--color-border-default)] grid h-9 w-9 place-items-center rounded-full font-medium">
+            {initialName}
+          </span>
+          <ul>
+            <li className="mt-1 text-gray-900">
+              <span>User ID: {user.id}</span>
+            </li>
+            <li className="mt-1 text-gray-900">
+              <span>User Email: {user.email}</span>
+            </li>
+            <li className="mt-1 text-gray-900">
+              <span>User Name: {user.name}</span>
+            </li>
+          </ul>
+        </Wrapper>
+
+        <Wrapper>
+          User Project: null
+          <Button onClick={handleLogout} variant="danger">
+            Logout
+          </Button>
+        </Wrapper>
       </div>
     </section>
   );
