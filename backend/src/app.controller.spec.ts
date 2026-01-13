b6324bf206f14f429bd75285fb4917a73405fcc6
linobollansee <linobollansee@hotmail.com>
2026-01-13 09:40:56 +0100

    KAN-46 Fix: Test Backend

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,10 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello from Collab Task Hub! Hot reload is working! 🚀"', () => {
-      expect(appController.getHello()).toBe(
-        'Hello from Collab Task Hub! Hot reload is working! 🚀',
-      );
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -20,6 +20,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello from Collab Task Hub! Hot reload is working! 🚀');
+      .expect('Hello World!');
   });
 });
