4212f181c70fb776ac0ee31721f381c1d340816f
linobollansee <linobollansee@hotmail.com>
2026-01-23 15:26:16 +0100

    SSL config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -28,6 +28,12 @@ import { ChatModule } from './chat/chat.module';
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: configService.get('NODE_ENV') === 'development',
         logging: true,
+        ssl:
+          configService.get('NODE_ENV') === 'production'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
       }),
       inject: [ConfigService],
     }),
